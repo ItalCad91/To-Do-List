2e0acd0c5e5a250873f44b8612b49e945dfc8cea
Riccardo Reali <rreali@my.centennialcollege.ca>
2023-06-21 17:23:15 -0400

    first commit

---
+++ src/components/TodoList.tsx
@@ -0,0 +1,40 @@
+import React, { useState } from "react";
+
+function TodoList() {
+  const [todos] = useState([
+    "Buy groceries",
+    "Clean the house",
+    "Walk the dog",
+  ]);
+  const [selectedTodoIndex, setSelectedTodoIndex] = useState<number | null>(
+    null
+  );
+
+  const handleClick = (index: number) => {
+    setSelectedTodoIndex(index);
+  };
+
+  return (
+    <div>
+      {todos.length > 0 ? (
+        <ul className="list-group">
+          {todos.map((todo, index) => (
+            <li
+              className={`list-group-item ${
+                selectedTodoIndex === index ? "active" : ""
+              }`}
+              key={index}
+              onClick={() => handleClick(index)}
+            >
+              {todo}
+            </li>
+          ))}
+        </ul>
+      ) : (
+        <p>No to-dos</p>
+      )}
+    </div>
+  );
+}
+
+export default TodoList;
